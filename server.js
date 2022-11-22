ee90fd23f5d0ea9c1de730e768b0ae07b9febd0a
pulsar1812 <cluster000@gmail.com>
2022-11-23 00:10:01 +0800

    Initial commit

---
+++ server.js
@@ -0,0 +1,34 @@
+const path = require('path')
+const express = require('express')
+const dotenv = require('dotenv')
+
+const connectDB = require('./config/db')
+
+dotenv.config()
+
+const app = express()
+
+// Connect DB
+connectDB()
+
+// Init Middleware
+app.use(express.json({ extended: false }))
+
+// Define Routes
+app.use('/api/users', require('./routes/users'))
+app.use('/api/auth', require('./routes/auth'))
+app.use('/api/contacts', require('./routes/contacts'))
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'))
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
